1fc333cac9d9900aeb255a8aaf8aea573c61f42b
Udai Singh Mehra <udai_mehra_2000@yahoo.com>
2025-02-11 10:56:23 +0530

    Added Assn_13.1.py and code3 directory

---
+++ assn_13.1.py
@@ -0,0 +1,41 @@
+#Extracting Data from XML
+
+#In this assignment you will write a Python program somewhat 
+#similar to http://www.py4e.com/code3/xml3.py. 
+#The program will prompt for a URL, read the XML data from that 
+#URL using urllib and then parse and extract the comment counts 
+#from the XML data, compute the sum of the numbers in the file.
+
+#We provide two files for this assignment. One is a sample file 
+#where we give you the sum for your testing and the other is the 
+#actual data you need to process for the assignment.
+
+#Sample data: http://py4e-data.dr-chuck.net/comments_42.xml (Sum=2553)
+#Actual data: http://py4e-data.dr-chuck.net/comments_2141809.xml (Sum ends with 42)
+
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = input('Enter location: ')
+if len(url) < 1 : 
+    #url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+    url = 'http://py4e-data.dr-chuck.net/comments_2141809.xml' 
+    #(For above URL Sum ends with 42)
+
+#print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+#print('Retrieved',len(data),'characters')
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+nums = list()
+#nums = []
+for result in counts:
+    # Debug print the data :)
+    #print(result.text)
+    if result.text:
+        nums.append(int(result.text))
+#print(nums)
+print('Count:', len(nums))
+print('Sum:', sum(nums))
