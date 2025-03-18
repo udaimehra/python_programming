dcb876fb4d410c4459cf258d6ac5c1f221ba8739
Udai Singh Mehra <udai_mehra_2000@yahoo.com>
2025-03-18 18:34:06 +0530

    Added another example in list_comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -2,3 +2,12 @@
 myList = list(range(100))
 FilteredList = [item for item in myList if item % 10 == 0]
 print(FilteredList)
+newString = 'My name is Udai Singh Mehra. I live in New Delhi.'
+newList = newString.replace('.', '').lower().split(' ')
+print(type(newList))
+list3 = []
+[list3.append(item) for item in newList if len(item) <=3]
+for item in newList:
+    if len(item) <=3:
+        print(item)
+print(list3)
