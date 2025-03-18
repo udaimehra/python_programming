88d0a262a5c21013927584e96a8f2833d7b1f213
Udai Singh Mehra <udai_mehra_2000@yahoo.com>
2025-03-18 21:30:58 +0530

    Added another List Comprehension line

--- list_comprehension.py
+++ list_comprehension.py
@@ -2,12 +2,21 @@
 myList = list(range(100))
 FilteredList = [item for item in myList if item % 10 == 0]
 print(FilteredList)
-newString = 'My name is Udai Singh Mehra. I live in New Delhi.'
-newList = newString.replace('.', '').lower().split(' ')
-print(type(newList))
+
+newString = 'My name is Udai Singh Mehra. I live in New Delhi'
+newList = newString.replace('.', '').lower().split()
+
 list3 = []
 [list3.append(item) for item in newList if len(item) <=3]
-for item in newList:
-    if len(item) <=3:
-        print(item)
 print(list3)
+
+myList1 = []
+sent = []
+myList1 = [[sent] for sent in newString.split('.')] 
+print(type(myList1))
+print(myList1)
+
+myList = []
+myList = [[word for word in sentence.split()] for sentence in newString.split('.')] 
+print(type(myList))
+print(myList)
