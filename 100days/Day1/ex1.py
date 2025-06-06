1a326c8611071c99132e764ca8b3ddc919a78589
Udai Singh Mehra <udai_mehra_2000@yahoo.com>
2025-06-06 13:14:03 +0530

    Added password-generator program

---
+++ 100days/Day1/ex1.py
@@ -0,0 +1,1 @@
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
